5bd414838e3b64126f5c518eb860bec6b0d72490
Akmal Hameed <akmal.hameed@hiacuity.com>
2022-11-19 20:31:08 +0530

    Test changes

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1> FIB Calculator </h1>
+          <h1> Dhilshan Calculator </h1>
           <Link to="/"> Home </Link>
           <Link to="/otherpage"> Other Page </Link>
         </header>
